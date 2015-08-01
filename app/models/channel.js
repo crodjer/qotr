e363b372bff166f0f32b9095ddc37e323a2f1740
Rohan Jain <crodjer@gmail.com>
2015-08-01 16:51:41 +0530

    A channel with encrypt/decrypt support
    Signed-off-by: Rohan Jain <crodjer@gmail.com>

--- app/models/channel.js
+++ app/models/channel.js
@@ -1,5 +1,45 @@
 import Ember from 'ember';
+import shortid from 'npm:shortid';
+
+var ivSeparator = '|';
 
 export default Ember.Object.extend({
   id: null,
+
+  init: function () {
+    if (!this.id) {
+      this.id = shortid.generate();
+      this.salt = forge.random.getBytesSync(128);
+      this.password = shortid.generate();
+    }
+
+    this.id_b64 = forge.util.encode64(this.id);
+    this.key = forge.pkcs5.pbkdf2(this.password, this.salt, 32, 32);
+  },
+
+  encrypt: function (str) {
+    var iv = forge.random.getBytesSync(32),
+        cipher = forge.aes.startEncrypting(this.key, iv),
+        input = forge.util.createBuffer(str);
+    cipher.update(input);
+    cipher.finish();
+    return [this.id, iv, cipher.output.data].
+      map(forge.util.encode64).
+      join(ivSeparator);
+  },
+
+  decrypt: function (str) {
+    if (str.indexOf(this.id_b64) !== 0) {
+      return str;
+    }
+
+    var byteArray = str.split(ivSeparator).map(forge.util.decode64),
+        iv = byteArray[1],
+        text = byteArray[2],
+        ptext = forge.aes.startDecrypting(this.key, iv),
+        newBuffer = forge.util.createBuffer(text);
+    ptext.update(newBuffer);
+    ptext.finish();
+    return ptext.output.data;
+  }
 });
--- tests/unit/models/channel-test.js
+++ tests/unit/models/channel-test.js
@@ -1,4 +1,5 @@
 import { moduleFor, test } from 'ember-qunit';
+import shortid from 'npm:shortid';
 
 moduleFor('model:channel', 'Unit | Model | channel', {
   // Specify the other units that are required for this test.
@@ -6,9 +7,28 @@ moduleFor('model:channel', 'Unit | Model | channel', {
 });
 
 test('it can be created', function(assert) {
-  var channel = this.subject({
-    id: 'foo'
-  });
+  var channel = this.subject();
+  assert.ok(channel.salt);
+  assert.ok(channel.password);
+  assert.ok(channel.key);
+  assert.equal(channel.decrypt(channel.encrypt('foo')), 'foo');
+});
 
-  assert.equal(channel.id, 'foo');
+test('it can be loaded', function(assert) {
+  var obj = {
+      id: shortid.generate(),
+      salt: forge.random.getBytesSync(128),
+      password: shortid.generate()
+  };
+  var channel = this.subject(obj);
+
+  assert.equal(obj.id, channel.id);
+  assert.equal(obj.salt, channel.salt);
+  assert.equal(obj.password, channel.password);
+  assert.equal(channel.decrypt(channel.encrypt('foo')), 'foo');
+});
+
+test('it ignores text which isn\'t encrypted', function(assert) {
+  var channel = this.subject();
+  assert.equal(channel.decrypt('foo'), 'foo');
 });
