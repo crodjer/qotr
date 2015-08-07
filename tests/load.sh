20eb190135d81852ef6673fc00cb51f14485160b
Rohan Jain <crodjer@gmail.com>
2015-08-07 17:27:59 +0530

    Load testing: thor knows when to stop
    Signed-off-by: Rohan Jain <crodjer@gmail.com>

--- tests/load.sh
+++ tests/load.sh
@@ -2,7 +2,6 @@
 
 HOST=$1
 COUNT=${2-2000}
-TIMEOUT=300
 PROJECT_DIR=$(cd $(dirname "${BASH_SOURCE[0]}")/../ && pwd)
 CHANNEL_ID=foo
 SALT=common
@@ -21,8 +20,9 @@ curl -vX POST $HOST/channels/new -d id=$CHANNEL_ID\&salt=$SALT &> /dev/null
 echo "Channel path: $HOST/c/$CHANNEL_ID"
 
 ws_path="$(echo $HOST | sed 's/^http/ws/g')/channels/$CHANNEL_ID"
+thor=$PROJECT_DIR/node_modules/thor/bin/thor
 
-timeout $TIMEOUT $PROJECT_DIR/node_modules/thor/bin/thor \
+$thor \
     --amount $COUNT \
     --messages=1000 \
     --generator $PROJECT_DIR/tests/load-generator.js \
