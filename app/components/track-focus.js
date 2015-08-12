a8667a558cac63a553b5aaeb98fe9de343394959
Rohan Jain <crodjer@gmail.com>
2015-08-12 11:01:55 +0530

    Remove window focus handlers after chat close
    Signed-off-by: Rohan Jain <crodjer@gmail.com>

--- app/components/track-focus.js
+++ app/components/track-focus.js
@@ -2,15 +2,25 @@ import Ember from 'ember';
 
 export default Ember.Component.extend({
   didInsertElement: function () {
-    var that = this,
-        winEl = Ember.$(window);
+    var winEl = Ember.$(window),
+        that = this;
 
-    winEl.focus(function () {
+    this.onFocus = function () {
       that.set('focused', true);
-    });
+    };
 
-    winEl.focusout(function () {
+    this.onFocusOut = function () {
       that.set('focused', false);
-    });
+    };
+
+    winEl.on('focus', this.onFocus);
+    winEl.on('focusout', this.onFocusOut);
+  },
+
+  willDestroy: function () {
+    var winEl = Ember.$(window);
+
+    winEl.off('focus', this.onFocus);
+    winEl.off('focusout', this.onFocusOut);
   }
 });
