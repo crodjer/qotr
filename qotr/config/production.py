47a57465e96b9d1b08f1be5312ab9c453d0a5704
Rohan Jain <crodjer@gmail.com>
2015-08-12 09:38:40 +0530

    Fix production environment variables
    Signed-off-by: Rohan Jain <crodjer@gmail.com>

--- qotr/config/production.py
+++ qotr/config/production.py
@@ -7,9 +7,9 @@ class Production(Base):
     QOTR production configuration.
     '''
 
-    host = os.environ.get('HOST') or 'localhost'
+    host = os.environ.get('HOST', 'localhost')
     port = os.environ.get('PORT')
 
     debug = False
-    allowed_origin = os.environ.get('ALLOWED_ORIGIN')
+    allowed_origin = os.environ.get('CORS_ALLOWED_ORIGIN', '')
     redirect_to_https = True
